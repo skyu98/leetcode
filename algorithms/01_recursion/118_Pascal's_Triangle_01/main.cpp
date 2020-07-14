136ea9e6cdbada7ac774f83bb971a0084cad5896
unknown <2286229152@qq.com>
2020-07-14 11:33:12 +0800

    118

---
+++ algorithms/01_recursion/118_Pascal's_Triangle_01/main.cpp
@@ -0,0 +1,22 @@
+class Solution { 
+    public:
+    int solve(int i, int j){ 
+            if(j==0 || j==i) return 1;
+            return solve(i-1, j-1)+solve(i-1,j);
+        }
+    
+    vector<vector<int>> generate(int numRows) { 
+            vector<vector<int>> res;
+            if(numRows == 0) return res;
+            for(int i = 0;i<numRows;i++)
+            { 
+                        vector<int> tmp;
+                        for(int j=0; j<=i;j++)
+                        { 
+                                        tmp.push_back(solve(i, j));
+                                    }
+                        res.push_back(tmp);
+                    }
+            return res;
+        }
+};
---
+++ algorithms/01_recursion/118_Pascal's_Triangle_02/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:    
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows == 0) return res;
+        for(int i = 0;i<numRows;i++)
+        {
+            vector<int> tmp;
+            for(int j=0; j<=i;j++)
+            {
+                if(j==0 || j==i) tmp.push_back(1);
+                else
+                {
+                    tmp.push_back(res[i-1][j-1] + res[i-1][j]);
+                }
+            }
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
